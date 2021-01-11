20e0c4ca5bd8868302f5040f887019e29b3dc226
AjPakorn S <pktiddy@gmail.com>
2021-01-11 19:56:02 +0700

    Add files via upload

---
+++ DandTime.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DandTime {
+
+	public static void main(String[] args) {
+		LocalDate d = LocalDate.now();
+		LocalDateTime dt = LocalDateTime.now();
+		DateTimeFormatter fdt =  DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		String fdatetime = dt.format(fdt);
+		System.out.println("Local Date Time: "+ d);
+		System.out.println("Date and Time : " + dt);
+		System.out.println("After format Date and Time : " + fdatetime);
+
+	}
+
+}
